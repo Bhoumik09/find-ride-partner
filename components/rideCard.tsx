9e94d3685bf62be16f30345d122084575126e6e3
Bhoumik <Bhoumikchopra12@gmail.com>
2025-04-21 14:31:49 +0530

    fixed rides card button issue

--- components/rideCard.tsx
+++ components/rideCard.tsx
@@ -11,14 +11,14 @@ import { Rides } from "@/lib/types";
 type RideCardProps = {
     ride: Rides;
     deleteTheRide?: (id: string) => void;
-  };
-export const RideCard: React.FC<RideCardProps> = ({ ride , deleteTheRide}:{ride:Rides, deleteTheRide?:(id:string)=>void}) => {
-    const auth:AuthContextType = useAuth();
-    const userId:string|undefined = auth.authData?.userData?.id;
+};
+export const RideCard: React.FC<RideCardProps> = ({ ride, deleteTheRide }: { ride: Rides, deleteTheRide?: (id: string) => void }) => {
+    const auth: AuthContextType = useAuth();
+    const userId: string | undefined = auth.authData?.userData?.id;
     return (
-        <Card>
-            <CardContent>
-                <div className="flex flex-col gap-3">
+        <Card className="max-h-[600px]">
+            <CardContent className="h-full">
+                <div className="flex flex-col h-full gap-3">
                     <div className="flex justify-between">
                         <div className="flex">
                             <Avatar className="size-10 sm:size-16">
@@ -35,50 +35,51 @@ export const RideCard: React.FC<RideCardProps> = ({ ride , deleteTheRide}:{ride:
                             {ride.user.id === userId &&
                                 <DropdownMenu >
                                     <DropdownMenuTrigger asChild className="  p-2 outline-0 border-0   ">
-                                        <Button size={'sm'}   variant={'default'} >Menu</Button>
+                                        <Button size={'sm'} variant={'default'} >Menu</Button>
                                     </DropdownMenuTrigger>
                                     <DropdownMenuContent>
                                         <DropdownMenuLabel>My Ride</DropdownMenuLabel>
                                         <DropdownMenuSeparator />
                                         <DropdownMenuItem asChild><Link href={`/update-ride/${ride.id}`}>Update</Link></DropdownMenuItem>
-                                        <DropdownMenuItem onClick={()=>deleteTheRide(ride.id)}>Delete</DropdownMenuItem>
-                                      
+                                        <DropdownMenuItem onClick={() => deleteTheRide(ride.id)}>Delete</DropdownMenuItem>
+
                                     </DropdownMenuContent>
                                 </DropdownMenu>
                             }
                         </div>
                     </div>
 
+                    <div className="flex-col flex gap-2 h-full">
+                        <div className="flex items-start">
+                            <MapPin className="w-6 h-6 md:w-7 md:h-7" />
 
-                    <div className="flex items-start ">
-                        <MapPin className="w-6 h-6 md:w-7 md:h-7" />
+                            <div className="flex flex-col justify-start">
+                                <p className="inline-block px-2  font-semibold text-md sm:text-xl">{ride?.source?.name}</p>
+                                <p className="opacity-50 max-sm:text-sm ">to {ride?.destination?.name}</p>
+                            </div>
 
-                        <div className="flex flex-col justify-start">
-                            <p className="inline-block px-2  font-semibold text-md sm:text-xl">{ride?.source?.name}</p>
-                            <p className="opacity-50 max-sm:text-sm ">to {ride?.destination?.name}</p>
                         </div>
+                        <div className="flex gap-2 items-center">
+                            <Clock className="size-6 md:size-7" />
+                            <p className="max-sm:text-sm text-lg">At {new Date(ride.time!).toLocaleTimeString("en-GB", {
+                                minute: 'numeric',
+                                hour: '2-digit'
 
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <Clock className="size-6 md:size-7" />
-                        <p className="max-sm:text-sm text-lg">At {new Date(ride.time!).toLocaleTimeString("en-GB", {
-                            minute: 'numeric',
-                            hour: '2-digit'
+                            })} , {new Date(ride.date!).toLocaleDateString("en-GB", {
+                                day: "numeric",
+                                month: "long",
+                                year: "numeric",
 
-                        })} , {new Date(ride.date!).toLocaleDateString("en-GB", {
-                            day: "numeric",
-                            month: "long",
-                            year: "numeric",
-
-                        })} </p>
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <UsersIcon className="size-6 md:size-7" />
-                        <p className="max-sm:text-sm text-lg">{ride?.numberOfSeats}  Available </p>
-                    </div>
-                    <div className="flex gap-2 items-center">
-                        <CarTaxiFront className="size-6 md:size-7" />
-                        <p className="max-sm:text-sm text-lg"> {ride?.vehicle}</p>
+                            })} </p>
+                        </div>
+                        <div className="flex gap-2 items-center">
+                            <UsersIcon className="size-6 md:size-7" />
+                            <p className="max-sm:text-sm text-lg">{ride?.numberOfSeats}  Available </p>
+                        </div>
+                        <div className="flex gap-2 items-center">
+                            <CarTaxiFront className="size-6 md:size-7" />
+                            <p className="max-sm:text-sm text-lg"> {ride?.vehicle}</p>
+                        </div>
                     </div>
                     <Button asChild>
                         <Link href={`/ride-info/${ride.id}`}>View Ride</Link>
